dc906145fa4f5a7d6688e90a912610575b30c4b2
Ibraheem <rf.nadal.22@gmail.com>
2022-03-26 20:40:55 +0300

    added createLevelForm ,added level slice and reducer,added asyncHandler to levelRouter

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,8 +3,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import { Container } from "react-bootstrap";
 import { ToastContainer } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
 import Register from "./pages/Register";
+import CreateLevelForm from "./pages/CreateLevelForm";
 const App = () => {
   return (
     <>
@@ -14,7 +15,8 @@ const App = () => {
           <Container>
             <Routes>
               <Route path="/login" element={<Login />} />
-              <Route path="/register" element={<Register/>}/>
+              <Route path="/register" element={<Register />} />
+              <Route path="/level/create" element={<CreateLevelForm />} />
             </Routes>
           </Container>
         </main>
---
+++ frontend/src/features/level/levelSlice.js
@@ -0,0 +1,57 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import levelService from "./levelService";
+const initialState = {
+  isError: false,
+  isSuccess: false,
+  isLoading: false,
+  message: "",
+  level: null,
+};
+export const create = createAsyncThunk(
+  "level/create",
+  async (level, thunkAPI) => {
+    try {
+      return await levelService.create(level);
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
+export const levelSlice = createSlice({
+  name: "level",
+  initialState,
+  reducers: {
+    reset: (state) => {
+      state.isLoading = false;
+      state.isError = false;
+      state.isSuccess = false;
+      state.message = "";
+      state.level = null;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(create.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(create.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.level = action.payload;
+      })
+      .addCase(create.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload;
+        state.level = null;
+      });
+  },
+});
+export const { reset } = levelSlice.actions;
+export default levelSlice.reducer;
