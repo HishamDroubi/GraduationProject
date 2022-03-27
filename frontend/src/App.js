8ee82d99e6a9cfc522d2efeb8dee6ef7087494da
moodgoza <moodghoz@gmail.com>
2022-03-27 16:17:50 +0300

    added a Profile Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Register from "./pages/Register";
 import CreateLevelForm from "./pages/CreateLevelForm";
+import Profile from "./pages/Profile";
 const App = () => {
   return (
     <>
@@ -17,6 +18,7 @@ const App = () => {
               <Route path="/login" element={<Login />} />
               <Route path="/register" element={<Register />} />
               <Route path="/level/create" element={<CreateLevelForm />} />
+              <Route path="/profile" element={<Profile />} />
             </Routes>
           </Container>
         </main>
