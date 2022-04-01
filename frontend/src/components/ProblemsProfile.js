38fca7cd13f70b8d7cc2a07cb6634895d0b329a8
Ibraheem <rf.nadal.22@gmail.com>
2022-04-01 17:28:30 +0300

    added solved problem to profile

--- frontend/src/components/ProblemsProfile.js
+++ frontend/src/components/ProblemsProfile.js
@@ -1,43 +1,47 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { getProblemSolved } from '../features/profile/profileSlice';
-import Loader from './Loader';
-import Message from './Message';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import Divider from '@mui/material/Divider';
-import ListItemText from '@mui/material/ListItemText';
-import ListItemAvatar from '@mui/material/ListItemAvatar';
-import Avatar from '@mui/material/Avatar';
-import Typography from '@mui/material/Typography';
-import { Card } from 'react-bootstrap';
-import ProblemSubmission from './ProblemSubmission';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getProblemSolved, reset } from "../features/profile/profileSlice";
+import Loader from "./Loader";
+import Message from "./Message";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import Divider from "@mui/material/Divider";
+import ListItemText from "@mui/material/ListItemText";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import Avatar from "@mui/material/Avatar";
+import Typography from "@mui/material/Typography";
+import { Card } from "react-bootstrap";
+import ProblemSubmission from "./ProblemSubmission";
 
 const ProblemsProfile = (props) => {
   const userName = props.userName;
   const dispatch = useDispatch();
-  const {
-    isError,
-    isLoading,
-    isSuccess,
-    message,
-    problemSolved
-  } = useSelector(state => state.profile);
+  const { isError, isLoading, isSuccess, message, problemSolved } = useSelector(
+    (state) => state.profile
+  );
 
   useEffect(() => {
     const codeforcesInfo = async () => {
       await dispatch(getProblemSolved(userName));
-    }
+    };
     codeforcesInfo();
-
-  }, [dispatch, props.userName]);
+    return () => {
+      dispatch(reset());
+    };
+  }, [dispatch, userName]);
 
   console.log(problemSolved);
   return (
     <>
-      {isLoading ? (<Loader/>) : problemSolved.map(p => (<ProblemSubmission userName={userName} problem={p}/>))}
+      {isLoading ? (
+        <Loader />
+      ) : (
+        problemSolved.map((p) => (
+          <ProblemSubmission userName={userName} key={p._id} problem={p} />
+        ))
+      )}
     </>
-  )
-}
+  );
+};
 
-export default ProblemsProfile
+export default ProblemsProfile;
